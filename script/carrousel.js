af31eb04a4aeefe8e9cb7bb7e65a82e1206851e8
AuriEcheand <aecheand@gmail.com>
2022-07-05 15:14:50 -0400

    s7-c14 ajout variable carrousel__active au fichier carrousel.js

--- script/carrousel.js
+++ script/carrousel.js
@@ -5,10 +5,15 @@
 
     let elmGalerie__img = document.querySelectorAll('.galerie figure img')
     console.log(elmGalerie__img)
+    let elmCarrousel = document.querySelector('.carrousel')
+    let elmCarrousel__figure = document.querySelector('.carrousel__figure')
+    console.log(elmGalerie__img[0].getAttribute('src'))
 
     for (const img of elmGalerie__img){
         img.addEventListener('mousedown', function(){
-            console.log(img.getAttribute('src'))
+            console.log(this.getAttribute('src'))
+            elmCarrousel.classList.add('carrousel__active')
+            console.log(elmCarrousel.classList)
 
         })
     }
