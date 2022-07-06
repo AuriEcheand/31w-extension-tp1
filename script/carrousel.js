56004237b4aff75ea90d83b8fd7f172dc2437fc7
AuriEcheand <99972357+AuriEcheand@users.noreply.github.com>
2022-07-06 16:29:57 -0400

    s8-c14 ajout de commentaire et écouter sur la galerie pour ouvrir le carrousel

--- script/carrousel.js
+++ script/carrousel.js
@@ -1,21 +1,26 @@
-(function(){
-    console.log("début du carrousel") 
-    //let elmGalerie = document.querySelector(".galerie")
-    //console.log(elmGalerie)
+/**
+ * Fonction IIFE qui permet de contrôler l'affichage du carrousel
+ * Auteur : Auristela Echeverria
+ * Date de dernière modification : 5 juillet 2022
+ * 
+ */
 
-    let elmGalerie__img = document.querySelectorAll('.galerie figure img')
-    console.log(elmGalerie__img)
+
+ (function(){
+    console.log('Début du carrousel')
+    let elmGalerie = document.querySelector('.galerie')
+    let elmGalerieImg = elmGalerie.querySelectorAll("figure figure img")
     let elmCarrousel = document.querySelector('.carrousel')
     let elmCarrousel__figure = document.querySelector('.carrousel__figure')
-    console.log(elmGalerie__img[0].getAttribute('src'))
-
-    for (const img of elmGalerie__img){
-            let elmImg = document.createAttribute()
-        //img.addEventListener('mousedown', function(){
-           // console.log(this.getAttribute('src'))
-            //elmCarrousel.classList.add('carrousel__active')
-            //console.log(elmCarrousel.classList)
-
-        })
+    let elmCarrousel__fermeture = document.querySelector('.carrousel__fermeture')
+    let elmCarrousel__radio = document.querySelector('.carrousel__radio')
+    console.log(elmGalerieImg[0].getAttribute('src'))
+    let index = 0 // L'index qui permettra d'indexer les images du carrousel et les radios bouton
+    for (const img of elmGalerieImg){
+        ajouter_elmImg(img)
+        ajouter_elmRadio()
+        // écouteur sur la galerie pour ouvrir le carrousel
+        img.addEventListener('mousedown', function(){
+            elmCarrousel.classList.add('carrousel__ouvrir') 
+    }) 
     }
- })()
